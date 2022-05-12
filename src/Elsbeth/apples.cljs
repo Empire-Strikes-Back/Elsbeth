bd7ac363880f47a1c8fe2c923c4da46350837cd2
Elsbeth <Elsbeth@how-about-a-hundred>
2022-05-12 14:13:43 +0300

    - i replace program drop-down menu with links - i properly process anchor clicks :James-Acaster good squirt his year!

--- src/Elsbeth/apples.cljs
+++ src/Elsbeth/apples.cljs
@@ -12,6 +12,7 @@
    [goog.string.format :as format]
    [goog.string :refer [format]]
    [goog.object]
+   [goog.events]
    [cljs.reader :refer [read-string]]
 
    [Elsbeth.seed]
@@ -25,6 +26,7 @@
    [reitit.frontend.easy :as Yzma.frontend.easy]
    [reitit.coercion.spec :as Yzma.coercion.spec]
    [reitit.frontend.controllers :as Yzma.frontend.controllers]
+   [reitit.frontend.history :as Yzma.frontend.history]
    [spec-tools.data-spec :as Yzma.data-spec]
 
    ["antd/lib/layout" :default ThemeSongGuyLayout]
@@ -44,6 +46,14 @@
    [:> ThemeSongGuyRow
     "a Jedi plagues me"]])
 
+(defn rc-game-page
+  [match stateA]
+  [:> (.-Content ThemeSongGuyLayout)
+   {:style {:background-color "white"}}
+   [:> ThemeSongGuyRow
+    "game"
+    #_(str "settings" (:rand-int @stateA))]])
+
 (defn rc-settings-page
   [match stateA]
   [:> (.-Content ThemeSongGuyLayout)
@@ -55,34 +65,34 @@
 (defn rc-current-page
   [matchA stateA]
   (Kuzco.core/with-let
-    []
-    [:> ThemeSongGuyLayout
-     [:> ThemeSongGuyMenu
-      {:mode "horizontal"
-       :key "program"
-       :size "small"
-       :defaultSelectedKeys []
-       :onClick (fn [event]
-                  (js/console.log event))
-       :items [{:label "program"
-                :key "program"
-                :icon nil
-                :children [{:type "group"
-                            :label (Kuzco.core/as-element
-                                    [:div {:style {:cursor "pointer"}
-                                           :onClick (fn [_] (Yzma.frontend.easy/push-state :rc-main-page))} "game"])
-                            #_(Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-main-page)} "game"])
-                            :key "game"
-                            :icon nil}
-                           {:type "group"
-                            :label (Kuzco.core/as-element
-                                    [:div {:style {:cursor "pointer"}
-                                           :onClick (fn [_] (Yzma.frontend.easy/push-state :rc-settings-page))} "settings"])
-                            #_(Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-main-page)} "settings"])
-                            :key "settings"
-                            :icon nil}]}]}]
-     (when-let [match @matchA]
-       [(-> match :data :view) match stateA])]
+    [route-keyA (Kuzco.core/cursor matchA [:data :name])]
+    (let [route-key @route-keyA]
+      [:> ThemeSongGuyLayout
+       [:> ThemeSongGuyMenu
+        {:mode "horizontal"
+         :size "large"
+         :selectedKeys [route-key]
+         :items [{:type "item"
+                  :label
+                  (Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-main-page)} "program"])
+                  :key :rc-main-page
+                  :icon nil}
+                 {:type "item"
+                  :label
+                  (Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-game-page)} "game"])
+                  :key :rc-game-page
+                  :icon nil}
+                 {:type "item"
+                  :label
+                  (Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-settings-page)} "settings"])
+                  :key :rc-settings-page
+                  :icon nil}]
+         #_[{:label "program"
+             :key "program"
+             :icon nil
+             :children []}]}]
+       (when-let [match @matchA]
+         [(-> match :data :view) match stateA])])
     #_[:<>
 
        [:ul
@@ -94,37 +104,59 @@
 (defn ui-process
   [{:keys [Pacha-dom-root
            matchA
-           stateA
-           rerender-rootA]
+           stateA]
     :as opts}]
-  (let []
-    (Yzma.frontend.easy/start!
-     (Yzma.frontend/router
-      ["/"
-       [""
-        {:name :rc-main-page
-         :view rc-main-page
-         :controllers [{:start (fn [_]
-                                 (js/console.log "start rc-main-page"))
-                        :stop (fn [_]
-                                (js/console.log "stop rc-main-page"))}]}]
-       ["setting"
-      ;; Shared data for sub-routes
-        {:name :rc-settings-page
-         :view rc-settings-page
-         :controllers [{:start (fn [_]
-                                 (js/console.log "start rc-settings-page"))
-                        :stop (fn [_]
-                                (js/console.log "stop rc-settings-page"))}]}]]
-      {:data {:controllers [{:start (fn [_]
-                                      (js/console.log "start root"))
-                             :stop (fn [_]
-                                     (js/console.log "stop root"))}]
-              :coercion Yzma.coercion.spec/coercion}})
-     (fn [new-match]
-       (swap! matchA (fn [old-match]
-                       (if new-match
-                         (assoc new-match :controllers (Yzma.frontend.controllers/apply-controllers (:controllers old-match) new-match))))))
-     {:use-fragment false})
+  (let [history (Yzma.frontend.easy/start!
+                 (Yzma.frontend/router
+                  ["/"
+                   [""
+                    {:name :rc-main-page
+                     :view rc-main-page
+                     :controllers [{:start (fn [_]
+                                             (js/console.log "start rc-main-page"))
+                                    :stop (fn [_]
+                                            (js/console.log "stop rc-main-page"))}]}]
+
+                   ["game"
+                    {:name :rc-game-page
+                     :view rc-game-page
+                     :controllers [{:start (fn [_]
+                                             (js/console.log "start rc-game-page"))
+                                    :stop (fn [_]
+                                            (js/console.log "stop rc-game-page"))}]}]
+                   ["setting"
+                    {:name :rc-settings-page
+                     :view rc-settings-page
+                     :controllers [{:start (fn [_]
+                                             (js/console.log "start rc-settings-page"))
+                                    :stop (fn [_]
+                                            (js/console.log "stop rc-settings-page"))}]}]]
+                  {:data {:controllers [{:start (fn [_]
+                                                  (js/console.log "start program"))
+                                         :stop (fn [_]
+                                                 (js/console.log "stop program"))}]
+                          :coercion Yzma.coercion.spec/coercion}})
+                 (fn [new-match]
+                   (swap! matchA (fn [old-match]
+                                   (if new-match
+                                     (assoc new-match :controllers (Yzma.frontend.controllers/apply-controllers (:controllers old-match) new-match))))))
+                 {:use-fragment false})]
+    (goog.events/unlistenByKey (:click-listen-key history))
+    (goog.events/listen js/document goog.events.EventType.CLICK
+                        (fn [event]
+                          (when-let [element (Yzma.frontend.history/closest-by-tag
+                                              (Yzma.frontend.history/event-target event) "a")]
+                            (let [uri (.parse goog.Uri (.-href element))]
+                              (when (Yzma.frontend.history/ignore-anchor-click? (.-router history) event element uri)
+                                (.preventDefault event)
+                                (let [path (str (.getPath uri)
+                                                (when (.hasQuery uri)
+                                                  (str "?" (.getQuery uri)))
+                                                (when (.hasFragment uri)
+                                                  (str "#" (.getFragment uri))))]
+                                  (.pushState js/window.history nil "" path)
+                                  (Yzma.frontend.history/-on-navigate history path)))))) true)
+
+
     #_(Yzma.frontend.easy/push-state :rc-main-page)
     (.render Pacha-dom-root (Kuzco.core/as-element [rc-current-page matchA stateA]))))
